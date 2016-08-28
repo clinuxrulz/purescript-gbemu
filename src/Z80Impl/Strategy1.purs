63c83d50c6998c398df73cb7af34c2eb6a8d6f5c
Clinton Selke <clinuxrulz@gmail.com>
2016-08-28 18:50:44 +1000

    started on suggestion 2

---
+++ src/Z80Impl/Strategy1.purs
@@ -0,0 +1,23 @@
+module Z80Impl.Strategy1 where
+
+-- Strategy1: Zero param effectful thunk array.
+
+import Z80Monoid
+
+import Prelude
+import Control.Monad.Eff (Eff)
+
+foreign import data Z80IO :: !
+foreign import data Z80State :: *
+
+-- Z80 instruction intermediate representation
+data Z80InstrIR =
+  Z80InstrIR
+    Int     -- ^ instruction size in bytes
+    Int     -- ^ number of cycles the instruction should take
+    (Z80State -> forall e. Eff (z80io :: Z80IO | e) Unit)
+
+-- Z80 intermediate representation
+data Z80IR =
+  Z80IR
+    (Array Z80InstrIR)
